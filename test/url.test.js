1d32e4903f2d5fbf7afadae9bf2d55f2d6849877
Denis Seleznev <hcodes@yandex.ru>
2020-04-12 11:00:15 +0300

    Split tests

---
+++ test/url.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const assert = require('chai').assert;
+const { processUrl } = require('../bin/utils');
+const exitCodes = require('../bin/exit-codes');
+
+describe('processUrl', function() {
+    this.timeout(15000);
+
+    const program = require('commander');
+    const oldExitCode = process.exitCode;
+    const oldLint = program.lint;
+
+    beforeEach(() => {
+        program.lint = true;
+        process.exitCode = oldExitCode;
+    });
+
+    afterEach(() => {
+        program.lint = oldLint;
+        process.exitCode = oldExitCode;
+    });
+
+    it('should set exit code ERROR_LOADING', done => {
+        processUrl('https://raw.githubusercontent.com/hcodes/utils/master/test/texts/unknown.txt').then(() => {
+            assert.equal(process.exitCode, exitCodes.ERROR_LOADING);
+            done();
+        });
+    });
+
+    it('should set exit code HAS_REPLACEMENT', done => {
+        processUrl('https://raw.githubusercontent.com/hcodes/eyo/master/test/texts/example_with_yo.txt').then(() => {
+            assert.equal(process.exitCode, exitCodes.HAS_REPLACEMENT);
+            done();
+        });
+    });
+
+    it('should not set exit code', done => {
+        processUrl('https://raw.githubusercontent.com/hcodes/eyo/master/test/texts/example_without_yo.txt').then(() => {
+            assert.equal(process.exitCode, oldExitCode);
+            done();
+        });
+    });
+});
